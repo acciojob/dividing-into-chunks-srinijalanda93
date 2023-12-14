7c7f3e4ce51190c14a8f64ab307a5367b3a52b85
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-12-14 16:47:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  let trackSum=0 ,ans=[] ,subArr=[];
+	//corner case
+	if(arr.length===0) return [];
+	for(int i=0;i<arr.length;i++){
+		if(trackSum+arr[i] <=n){
+			//push in the subArr
+			subArr.push(arr[i]);
+			//update the trackSum
+			trackSum=trackSum+arr[i];
+		}
+		else{
+			ans.push(subArr)
+			trackSum=0;
+			subArr=arr[i];
+		}
+	}
+	return ans.push(subArr);
 };
 
 const n = prompt("Enter n: ");
