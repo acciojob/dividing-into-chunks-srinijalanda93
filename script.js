a5556fbed17ec6d45627b4e4a01e9c4ae5bf0918
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-12-14 16:58:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  let trackSum=0 ,ans=[] ,subArr=[];
-	//corner case
-	if(arr.length===0) return [];
-	for(let i=0;i<arr.length;i++){
-		if(trackSum+arr[i] <=n){
-			//push in the subArr
-			subArr.push(arr[i]);
-			//update the trackSum
-			trackSum=trackSum+arr[i];
-		}
-		else{
-			ans.push(subArr)
-			trackSum=0;
-			subArr = [arr[i]];
-		}
-	}
-	ans.push(subArr);
+  let trackSum = 0, ans = [], subArr = [];
+  
+  // Corner case
+  if (arr.length === 0) return [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (trackSum + arr[i] <= n) {
+      // Push in the subArr
+      subArr.push(arr[i]);
+      // Update the trackSum
+      trackSum = trackSum + arr[i];
+    } else {
+      ans.push(subArr);
+      trackSum = arr[i]; // Start a new trackSum
+      subArr = [arr[i]]; // Start a new subArr with the current element
+    }
+  }
+  
+  // Push the last subArr to ans
+  ans.push(subArr);
   
   return ans;
 };
